3284e31f7766d5218388b04b22f6ba2edc7e5d43
telrob <telrob@126.com>
2019-06-04 01:58:24 -0700

    对串口命令进行接收

--- main.c
+++ main.c
@@ -6,14 +6,23 @@ void u_call(char*data,int len)
 {
     if(len>=4)
     {
-        char length=data[0];
+        unsigned char length=data[0];
         char cc=data[1];
-        short cmd=(data[2]&0xff)|((data[3]&0xff)<<8);
+        unsigned short cmd=(data[2]&0xff)|((data[3]&0xff)<<8);
         if(length+4==len&&sm_crc(data+4,length)==cc)
         {
             if(cmd==0)
             {
                 printf("log:%s\n",data+4);
+            }
+            else if(cmd==1)
+            {
+                unsigned char*datas=data+4;
+                printf("mac:%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X\n",datas[0],datas[1],datas[2],
+                datas[3],datas[4],datas[5],datas[6],datas[7]);
+                unsigned short idadd=(short)(datas[8]&0xFF)|((datas[9]&0xFF)<<8);
+                printf("short addr:%04x %u\n",idadd,idadd);
+                
             }
         }
         else
@@ -37,7 +46,7 @@ int main()
          char*str="gggggg";
         while(1)
         {
-            sm_uart_write(str,strlen(str));
+            //sm_uart_write(str,strlen(str));
             sleep(2);
         }
     }else{
